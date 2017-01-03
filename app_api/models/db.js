22b8cd1926fb85a53ae47bacd80b6f2dc5911964
wym7230 <wym7230@Gmail.com>
2017-01-02 23:16:46 -0800

    revised an minor error

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -46,7 +46,7 @@ process.on('SIGINT', function() {
         process.exit(0);
     });
 });
-For Heroku app termination
+//For Heroku app termination
 process.on('SIGTERM', function() {
     gracefulShutdown('Heroku app termination', function() {
         process.exit(0);
